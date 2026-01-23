d72b99451587fc3de2ee0f3562463f9e625ded33
“Sameer <sameeransariscience@gmail.com>
2026-01-23 10:35:44 +0530

    Project

--- React/2Lec/src/App.jsx
+++ React/2Lec/src/App.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+  console.log("App component rendered");
+  
+  let url = 'https://jsonplaceholder.typicode.com/posts';
+      // fetch(url) 
+      // .then(res => res.json())
+      // .then(data => {
+      //   console.log(data)
+      // setTodos(data)})
+      // .catch(err => console.log(err))
+
+      useEffect( () => {
+        console.log("useEffect called");
+      fetch(url) 
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      setTodos(data)})
+      .catch(err => console.log(err))
+      }, [])
   return (
-    <div>App</div>
+    <div>
+      {todos.map(t => <div key={t.id}>{t.title} - {t.body}</div>)}
+    </div>
   )
 }
 
